fc1d7b064dc15e2fc6dbabd7e8c0325bdf51646a
Li-Sung Ou <Peter1031734590@gmail.com>
2022-08-08 16:52:45 +1000

    Rewrite functions with stricter access controls.

--- tic_tac_toe_functional_programming_approach.js
+++ tic_tac_toe_functional_programming_approach.js
@@ -11,12 +11,6 @@ const outOfBounds = 'Position out of bounds. Please try again.';
 const positionFilled = 'Position already filled. Please try again.';
 const drawMessage = 'The game ended in a draw.';
 const replayPrompt = 'Do you wish to play again? [y/N] ';
-let movePrompt = (currentPlayer) => `Player ${currentPlayer}, please enter the index of your next move: `;
-let winMessage = (winner) => `\nPlayer ${winner} has won the game.\n\n`;
-let loseMessage = (player) => `\nAnd player ${player} is the\n\n`
-+ `██       ██████  ███████ ███████ ██████  \n` + `██      ██    ██ ██      ██      ██   ██ \n` + `██      ██    ██ ███████ █████   ██████  \n`
-+ `██      ██    ██      ██ ██      ██   ██ \n` + `███████  ██████  ███████ ███████ ██   ██ \n`;
-
 
 const winningTrios = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], 
@@ -24,106 +18,88 @@ const winningTrios = [
     [0, 4, 8], [2, 4, 6]
 ];
 
-let playerOnesTurn = true;
-let board = new Array(boardLength ** 2).fill(empty);
-let winner = null;
-
 // Prints the current board out to the terminal.
     
 //     Parameters:
 //         board: A list containing the values for each position on the board.
 //         padding: How many spaces should appear either side of the values.
-function printGameboard (board, padding = 1) {
+function printGameBoard (board, padding = 1) {
     const horizontal_divider = '-'.repeat(boardLength * (2 * padding + 1) + 2);
     console.log("\n");
 
     //Divide the array into rows based on the board length
-    for (let rowIndex = 0; rowIndex < boardLength; ++rowIndex) {
-        if (rowIndex != 0) {
+    for (let i = 0; i < boardLength; ++i) {
+        if (i != 0) {
             console.log(horizontal_divider);
         }
-        let start = rowIndex * boardLength;
+        let start = i * boardLength;
         let row = board.slice(start, start + boardLength);
-        row = row.map(item => `${empty.repeat(padding)}${item}${empty.repeat(padding)}`).join("|");
+        row = row.map(item => `${empty.repeat(padding)}${item}${empty.repeat(padding)}`);
 
-        console.log(row);
+        console.log(row.join("|"));
     }
     console.log("\n");
 }
 
-function reset() {
+function reset(playerOnesTurn, board, winner) {
     playerOnesTurn = true;
     board = new Array(boardLength ** 2).fill(empty);
     winner = null;
 }
 
-function getNextMove() {
+function getNextMove(playerOnesTurn) {
+    let movePrompt = (currentPlayer) => `Player ${currentPlayer}, please enter the index of your next move: `;
     while (true) {
-       let move = prompt( movePrompt( getCurrentPlayer() ) );
+       let move = prompt( movePrompt( getCurrentPlayer(playerOnesTurn) ) );
 
         //Type check the input value.
        if (typeof Number(move) === 'number') {
         return Number(move);
-       }
-
+       } 
        console.log(invalidInput);
     }
 }
 
-function getCurrentPlayer() {
+function getCurrentPlayer(playerOnesTurn) {
     return (playerOnesTurn ? playerOne : playerTwo);
 }
 
-function getLoser() {
+function getLoser(playerOnesTurn) {
     return (playerOnesTurn ? playerTwo : playerOne);
 }
 
 //Check if the current player has won the game.
-function check_win() {
+function checkWin(board, playerOnesTurn) {
 
     //Get all of the indices of current_player's moves.
     let positions = board.map((value, index) => {return {value, index}})
-    .filter(entries => entries.value == getCurrentPlayer())
+    .filter(entries => entries.value == getCurrentPlayer(playerOnesTurn))
     .map(entries => entries.index);
-
 
     // Check if the player is at all indicies in a winning trio
     if (winningTrios.some(trio => trio.every(item => positions.includes(item)))) {
         return true;
     }
-    
     return false;
 }
 
-//Returns true iff neither player is able to make a move.
-function hasDrawn() {
+function hasDrawn(board) {
     return !(board.includes(empty));
 }
 
-function isOver() {
-    return (hasDrawn() || winner != null);
+function isOver(board, winner) {
+    return (hasDrawn(board) || winner != null);
 }
 
-function display_winner_info() {
-    if (winner === null) {
-        printGameboard(board);
-        console.log(drawMessage);
-    } else {
-        console.clear();
-        printGameboard(board);
-        console.log(winMessage(winner));
-        console.log(loseMessage(getLoser()));
-    }
-}
+function play(board, winner, playerOnesTurn) {
 
-function play() {
-    while (!isOver()) {
-        printGameboard(board);
+    while ( !isOver(board, winner) ) {
+        printGameBoard(board);
 
-        let index = getNextMove();
+        let index = getNextMove(playerOnesTurn);
 
-         // Perform out of bounds checks
-         if (!(0 <= index < boardLength ** 2)) {
+        // Perform out of bounds checks
+        if ( !(0 <= index < boardLength ** 2) ) {
             console.log(outOfBounds);
             continue;
         }
@@ -136,12 +112,14 @@ function play() {
         }
 
         // Enact the move on the board
-        board[index] = getCurrentPlayer();
+        board[index] = getCurrentPlayer(playerOnesTurn);
 
         // Check for end game conditions
-        if (check_win()) {
-            winner = getCurrentPlayer();
-            return;
+        if ( checkWin(board, playerOnesTurn) ) {
+            winner = getCurrentPlayer(playerOnesTurn);   
+            return winner;
+        } else if ( hasDrawn(board) ) {
+            return null;
         }
 
         // Flip the turn
@@ -149,11 +127,35 @@ function play() {
     }
 }
 
+function displayWinnerInfo(winner, playerOnesTurn, board) {
+    let winMessage = (winner) => `\nPlayer ${winner} has won the game.\n\n`;
+    let loseMessage = (player) => `\nAnd player ${player} is the\n\n`
+    + `██       ██████  ███████ ███████ ██████  \n` + `██      ██    ██ ██      ██      ██   ██ \n` + `██      ██    ██ ███████ █████   ██████  \n`
+    + `██      ██    ██      ██ ██      ██   ██ \n` + `███████  ██████  ███████ ███████ ██   ██ \n`;
+    if (winner === null) {
+        printGameBoard(board);
+        console.log(drawMessage);
+    } else {
+        console.clear();
+        printGameBoard(board);
+        console.log( winMessage(winner) );
+        console.log( loseMessage(getLoser(playerOnesTurn)) );
+    }
+}
 
-function main(){
+    function game(board, playerOnesTurn, winner) {
+        displayWinnerInfo( play(board, winner, playerOnesTurn), playerOnesTurn, board);
+    }
+
+/***************/
+function main() {
+    let playerOnesTurn = true;
+    let board = new Array(boardLength ** 2).fill(empty);
+    let winner = null;
+
     while (true) {
-        play();
-        display_winner_info();
+        game(board, playerOnesTurn, winner);
+
         // Ask user if they want to play again
         if (prompt(replayPrompt) != 'y') {
             console.clear();
@@ -170,7 +172,7 @@ function main(){
             console.log("\n\n\n\n\n\n\n\n\n");
         break;
         }
-        reset();
+        reset(playerOnesTurn, board, winner);
     }
 }
 
